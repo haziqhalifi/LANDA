0c65d853da2eb731889de9b7aa968fddc27366e5
ishida-san <122435077+Aliff00@users.noreply.github.com>
2026-03-09 20:40:13 +0800

    Delete flutter_bootstrap.js

--- frontend_flutter/disaster_resilience_ai/web/flutter_bootstrap.js
+++
@@ -1,9 +0,0 @@
-{{flutter_js}}
-{{flutter_build_config}}
-
-// Force SKWasm renderer to avoid CanvasKit context-lost runtime crashes.
-_flutter.loader.load({
-  config: {
-    renderer: "skwasm",
-  },
-});
